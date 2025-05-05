8634ac3a87b42b055e93602edf79a4e193dbc543
NethuDinsara <neth.hettihewa@gmail.com>
2025-05-06 01:35:15 +0530

    updated app

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Hero from './components/Hero';
 import About from './components/About';
 import Skills from './components/Skills';
 import Projects from './components/Projects';
-import ExtraCurricular from './components/Extracurricular';
+import ExtraCurricular from './components/ExtraCurricular';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
 import './index.css';
